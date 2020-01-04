2c3764cf8420d506e86ddee56d1ddaa9559d4868
ropturpvp <59321351+ropturpvp@users.noreply.github.com>
2020-01-04 11:07:33 -0300

    0.4

---
+++ myjs/saddles.js
@@ -0,0 +1,200 @@
+function sendsaddles(option){
+	switch (option)
+	{
+		case 1:
+				document.getElementById("set").value = "Argentavis";
+				break;
+
+		case 2:
+				document.getElementById("set").value = "Ankylosaurus";
+				break;
+
+		case 3:
+				document.getElementById("set").value = "Castoroides";
+				break;
+				
+		case 4:
+				document.getElementById("set").value = "Doedicurus";
+				break;
+				
+		case 5:
+				document.getElementById("set").value = "Mantis";
+				break;
+				
+		case 6:
+				document.getElementById("set").value = "Yutyrannus";
+				break;
+				
+		case 7:
+				document.getElementById("set").value = "Unicorn";
+				break;
+				
+		case 8:
+				document.getElementById("set").value = "Therizinosaurus";
+				break;
+				
+		case 9:
+				document.getElementById("set").value = "Mammoth";
+				break;
+				
+		case 10:
+				document.getElementById("set").value = "Quetzal";
+				break;
+				
+		case 11:
+				document.getElementById("set").value = "QuetzalPlataforma";
+				break;
+				
+		case 12:
+				document.getElementById("set").value = "Pteranodon";
+				break;
+				
+		case 13:
+				document.getElementById("set").value = "Tapejara";
+				break;
+				
+		case 14:
+				document.getElementById("set").value = "Tapejaratek";
+				break;
+				
+		case 15:
+				document.getElementById("set").value = "Triceratops";
+				break;
+				
+		case 16:
+				document.getElementById("set").value = "Rockelemental";
+				break;
+				
+		case 17:
+				document.getElementById("set").value = "Daeodon";
+				break;
+				
+		case 18:
+				document.getElementById("set").value = "Thylacoleo";
+				break;
+				
+		case 19:
+				document.getElementById("set").value = "Woollyrhino";
+				break;
+				
+		case 20:
+				document.getElementById("set").value = "Procoptodon";
+				break;
+				
+		case 21:
+				document.getElementById("set").value = "Megatherium";
+				break;
+				
+		case 22:
+				document.getElementById("set").value = "Rex";
+				break;
+				
+		case 23:
+				document.getElementById("set").value = "Rextek";
+				break;
+				
+		case 24:
+				document.getElementById("set").value = "Spinosaur";
+				break;
+				
+		case 25:
+				document.getElementById("set").value = "Giganotosaurus";
+				break;
+				
+		case 26:
+				document.getElementById("set").value = "Beelzebufo";
+				break;
+				
+		case 27:
+				document.getElementById("set").value = "Tusoteuthis";
+				break;
+				
+		case 28:
+				document.getElementById("set").value = "Mosasaurus";
+				break;
+				
+		case 29:
+				document.getElementById("set").value = "MosasaurusPlataforma";
+				break;
+				
+		case 30:
+				document.getElementById("set").value = "Mosasaurustek";
+				break;
+				
+		case 31:
+				document.getElementById("set").value = "Plesiosaur";
+				break;
+				
+		case 32:
+				document.getElementById("set").value = "PlesiosaurPlataforma";
+				break;
+				
+		case 33:
+				document.getElementById("set").value = "Stegosaurus";
+				break;
+				
+		case 34:
+				document.getElementById("set").value = "Carbonemys";
+				break;
+				
+		case 35:
+				document.getElementById("set").value = "Megalodon";
+				break;
+				
+		case 36:
+				document.getElementById("set").value = "Megalodontek";
+				break;
+				
+		case 37:
+				document.getElementById("set").value = "Gacha";
+				break;
+				
+		case 38:
+				document.getElementById("set").value = "Managarmr";
+				break;
+				
+		case 39:
+				document.getElementById("set").value = "Gasbags";
+				break;
+				
+		case 40:
+				document.getElementById("set").value = "Snowowl";
+				break;
+				
+		case 41:
+				document.getElementById("set").value = "Velonasaur";
+				break;
+				
+		case 42:
+				document.getElementById("set").value = "Basilisk";
+				break;
+				
+		case 43:
+				document.getElementById("set").value = "Karkinos";
+				break;
+				
+		case 44:
+				document.getElementById("set").value = "RockDrake";
+				break;
+				
+		case 45:
+				document.getElementById("set").value = "RockDraketek";
+				break;
+				
+		case 46:
+				document.getElementById("set").value = "RollRat";
+				break;
+				
+		case 47:
+				document.getElementById("set").value = "Ravager";
+				break;
+				
+		case 48:
+				document.getElementById("set").value = "Brontosaurus";
+				break;
+				
+		case 49:
+				document.getElementById("set").value = "BrontosaurusPlataforma";
+				break;
+	}
+}
