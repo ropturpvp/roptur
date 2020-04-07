bac8487e598f46a46821be37e20cf775f9fef5c3
ropturpvp <59321351+ropturpvp@users.noreply.github.com>
2020-04-07 19:50:38 -0300

    Update properties.js

--- myjs/properties.js
+++ myjs/properties.js
@@ -1,2 +1,2 @@
-window.oncontextmenu=function(){ console.log("NO DEBERIAS ESTAR ACA!"); return false; }
+window.oncontextmenu=function(){ console.log('%c NO DEBERIAS ESTAR ACA!\n se notifico dicho acto al administrador', 'color: red; font-size: 40px;'); return false; }
 window.onselectstart=new Function("return false");
