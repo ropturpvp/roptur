5bde9db3778f20478e9806cdfc6fd232ca74a4ef
ropturpvp <59321351+ropturpvp@users.noreply.github.com>
2020-04-16 16:44:19 -0300

    Update properties.js

--- myjs/properties.js
+++ myjs/properties.js
@@ -1,5 +1,5 @@
-window.onload=function(){ console.clear(); console.log('%c NO DEBERIAS ESTAR ACA!\n Se notifico al Administrador', 'color: red; font-size: 40px;'); return false; }
-window.oncontextmenu=function(){ console.clear(); console.log('%c NO DEBERIAS ESTAR ACA!\n Se notifico al Administrador', 'color: red; font-size: 40px;'); return false; }
+window.onload=function(){ console.clear(); console.log('%c NO DEBERIA ESTAR ACA!', 'color: red; font-size: 40px;'); return false; }
+window.oncontextmenu=function(){ console.clear(); console.log('%c NO DEBERIA ESTAR ACA!', 'color: red; font-size: 40px;'); return false; }
 window.onselectstart=new Function("return false");
 
 let div = document.createElement('div');
