b512c3be3c76bdfddf69a342e7581565f336c346
ropturpvp <59321351+ropturpvp@users.noreply.github.com>
2020-04-16 19:30:38 -0300

    Update properties.js

--- myjs/properties.js
+++ myjs/properties.js
@@ -7,12 +7,14 @@ let loop = setInterval(() => {
 	console.log(div);
 	console.clear();
 }, 500);
-Object.defineProperty(div, "id", {get: () => { 
-	clearInterval(loop);
-	document.getElementsByTagName("html")[0].remove();
-	setTimeout(() => {
-		console.log('%c NO DEBERIA ESTAR ACA!\n Cierre la consola de desarrollador!', 'color: red; font-size: 40px;');
-	}, 500);
+Object.defineProperty(div,"id",{get:()=>
+{
+	clearInterval(loop); 
+	setTimeout(()=>document.getElementsByTagName("html")[0].remove(),500);
+	let left = (screen.width/2)-(370/2);
+  	let top = (screen.height/2)-(180/2);
+	let web=window.open("", 'Roptur', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=370,height=180,left = '+left+',top = '+top+'');
+	web.document.body.innerHTML="<h3>ROPTUR</h3><h3 style=\"color: #ff2f00; text-align: center;\">NO TIENE PERMITIDO ESTAR ACA!<br>Cierre las herramientas de desarrollador<br>y vuelva a cargar la pagina.</h3><div style=\"width: 25%;margin:0 auto;\"><button onclick=\"javascript:window.close()\">CERRAR</button></div>";
 }});
 
 if ( window.history.replaceState ) {
